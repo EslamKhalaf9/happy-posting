427015f9e78980c2c3d328fec88a4fa2767400a6
eslamkhalaf9 <eslamkhalaf191@gmail.com>
2020-05-02 15:15:51 +0200

    :tada: initial commit

---
+++ happy-posting/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <h1 className="brand">Hello World abo khalaf ya gamed</h1>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
